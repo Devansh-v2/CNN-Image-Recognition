f5837ab8834329098b625b4b18e13781788c9e80
Devansh Shukla <78256325+Devansh-v2@users.noreply.github.com>
2023-05-14 09:51:23 +0530

    Add files via upload

---
+++ CNN_Model.py
@@ -0,0 +1,33 @@
+# Import the required libraries
+
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Preprocess the data
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Define the model architecture
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=20, batch_size=64, validation_data=(x_test, y_test))
+
+# saving the model
+model.save('animal_model.json')
